9d7d9a1fef73600de7c2c6e551c74e469d980f23
Christina  Ng <cmatson93@gmail.com>
2021-05-04 18:39:14 -0700

    show welcome component if logged in other wise show login componet

--- pages/index.js
+++ pages/index.js
@@ -1,69 +1,44 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import Profile from '../src/components/Profile'
+
+import { useUser } from '@auth0/nextjs-auth0';
 
 export default function Home() {
+
+  const { user, error, isLoading } = useUser();
+  console.log(user)
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
+
+  // if (user) {
+  //   return (
+  //     <a href="/api/auth/login">Login</a>
+  //     <a href="/api/auth/logout">Logout</a>
+  //   )
+  // }
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-        <a href="/api/auth/login">Login</a>
-        <a href="/api/auth/logout">Logout</a>
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        {/* <Profile /> */}
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+      {
+        user ? 
+          <main className={styles.main}>
+            <h1 className={styles.title}>
+              Welcome to Grit!
+            </h1>
+          
+            <p className={styles.description}>
+              Get started crushing your goals!{' '}
             </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+            <a href="/api/auth/logout">Logout</a>
+          </main>
+        : 
+        <a href="/api/auth/login">Login</a>
+      }
     </div>
   )
 }
