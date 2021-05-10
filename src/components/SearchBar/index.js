8b09af6ce166364507476f7ef062228219cafad5
Tommy Ng <tommyng@Tommys-MacBook-Pro.local>
2021-05-09 20:28:58 -0700

    add community page

---
+++ src/components/SearchBar/index.js
@@ -0,0 +1,16 @@
+import Search from '../../icons/Search'
+import Voice from '../../icons/Voice'
+import styles from './search.module.scss'
+
+const SearchBar = ({ props }) => {
+
+    return (
+        <div className={styles.row}>
+            <Search className={styles.icon}/>
+            <input className={styles.inputField} type="text" placeholder="Search"></input>
+            <Voice className={styles.icon}/>
+        </div>
+    );
+}
+
+export default SearchBar; 
